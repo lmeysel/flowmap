708fa90bd7807855c16d3abc54bd69ca8994bcf2
Ludwig Meysel <lmeysel@users.noreply.github.com>
2017-07-03 10:44:09 +0200

    Project initialization

---
+++ src/rs/flowmap/Program.java
@@ -0,0 +1,18 @@
+package rs.flowmap;
+
+/**
+ * 
+ * @author Mitja Stachowiak, Ludwig Meysel
+ * 
+ * @version 03.07.2017
+ */
+public class Program {
+
+	/**
+	 * Creates a new object from the {@see Program} class.
+	 */
+	public Program() {
+
+	}
+
+}
