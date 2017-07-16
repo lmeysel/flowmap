89060a3b7fb6427fb071c1b5dcb16bf58e82d366
Ludwig Meysel <lmeysel@users.noreply.github.com>
2017-07-16 20:50:39 +0200

    some mor infos for the graph interfaces

--- src/rs/flowmap/graph/base/Edge.java
+++ src/rs/flowmap/graph/base/Edge.java
@@ -9,4 +9,13 @@ package rs.flowmap.graph.base;
 public interface Edge {
 	public Vertex getSource();
 	public Vertex getTarget();
+	
+	/**
+	 * Gets the edge's weight.
+	 */
+	public int getWeight();
+	/**
+	 * Sets the edge's weight.
+	 */
+	public void setWeight(int weight);
 }
